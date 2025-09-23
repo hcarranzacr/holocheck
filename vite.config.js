42f5f56a1d26b2fa3f389894a31ed117409a1636
hcarranzacr <hcarranzacr@example.com>
2025-09-23 00:33:51 -0700

    Vite server exit code fix.

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true,
-    strictPort: false
+    port: 3000,
+    host: '0.0.0.0',
+    strictPort: true
   },
   build: {
     outDir: 'dist',
